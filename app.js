6f73724a966327726a65aa6077d44da52f084c1b
0x00ahmad <mahmadrehan00@gmail.com>
2024-07-29 14:56:13 +0300

    added validation

--- app.js
+++ app.js
@@ -3,23 +3,6 @@ import * as course from "./courseController.js";
 
 const app = new Hono();
 
-// app.get("/", async (c) => {
-//   const template = await eta.render("index.eta", {});
-//   return c.html(template);
-// });
-//
-// app.post("/feedbacks/:id", async (c) => {
-//   const id = c.req.param("id");
-//   await feedbacks.addFeedback(id);
-//   return c.redirect("/");
-// });
-//
-// app.get("/feedbacks/:id", async (c) => {
-//   const id = c.req.param("id");
-//   const count = await feedbacks.getFeedbackCount(id);
-//   return c.text(`Feedback ${id}: ${count}`);
-// });
-
 app.get("/courses", course.getAllCourses);
 app.post("/courses", course.createCourse);
 app.get("/courses/:id", course.getCourseById);
--- courseController.js
+++ courseController.js
@@ -1,14 +1,31 @@
 import * as courseService from "./courseService.js";
 import { eta } from "./eta.js";
+import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
+
+const courseSchema = z.object({
+  name: z.string().min(4, {
+    message: "The course name should be a string of at least 4 characters.",
+  }),
+});
 
 export async function getAllCourses(c) {
   const courses = await courseService.getAllCourses();
-  return c.html(eta.render("courses.eta", { courses }));
+  return c.html(eta.render("courses.eta", { courses, prevData: { name: "" } }));
 }
 
 export async function createCourse(c) {
-  const { name } = await c.req.parseBody();
-  await courseService.addCourse(name);
+  const body = await c.req.parseBody();
+  const result = courseSchema.safeParse(body);
+  if (!result.success) {
+    return c.html(
+      eta.render("courses.eta", {
+        courses: await courseService.getAllCourses(),
+        prevData: body,
+        errors: result.error.format(),
+      }),
+    );
+  }
+  await courseService.addCourse(result.data.name);
   return c.redirect("/courses");
 }
 
